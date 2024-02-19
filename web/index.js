31111fe9f430ed778652f97777aed773491dfc33
lohopupa <lohopupa@gmail.com>
2024-02-19 15:27:22 +0600

    [WEB] Add background image and styles

--- web/index.js
+++ web/index.js
@@ -4,3 +4,13 @@ const go = new Go();
 WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
     go.run(result.instance);
 });
+
+const parallaxContainer = document.getElementById('parallaxContainer');
+const background = document.getElementById('background');
+
+document.addEventListener('mousemove', (event) => {
+    const mouseX = event.clientX / window.innerWidth - 0.5;
+    const mouseY = event.clientY / window.innerHeight - 0.5;
+
+    background.style.transform = `translate(${-mouseX * 50}px, ${-mouseY * 50}px)`;
+});
